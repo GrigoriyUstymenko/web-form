e2677b1d554ab38232b1aec9f52af65555a50d1c
GrigoriyUstymenko <gromgoriy@gmail.com>
2022-01-01 18:23:06 +0200

    Fixed send mail function

--- api/sendMail.js
+++ api/sendMail.js
@@ -33,66 +33,68 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export default async (req, res) => {
-  try {
-    const ip = req.headers['x-forwarded-for'];
-    console.log(ip);
+export default (req, res) => {
+  const ip = req.headers['x-forwarded-for'];
+  console.log(ip);
 
-    if (
-      !rateLimit(history, ip, defaultRateLimit.count, defaultRateLimit.time)
-    ) {
-      res.status(429).json({
-        errors: [
-          {
-            id: Date.now(),
-            status: '429',
-            title: 'Too many requests',
-            detail: 'request limit exceeded, try again later',
-          },
-        ],
-      });
-      return;
-    }
+  if (!rateLimit(history, ip, defaultRateLimit.count, defaultRateLimit.time)) {
+    res.status(429).json({
+      errors: [
+        {
+          id: Date.now(),
+          status: '429',
+          title: 'Too many requests',
+          detail: 'request limit exceeded, try again later',
+        },
+      ],
+    });
+    return;
+  }
 
-    if (!req.body.name || !req.body.email || !req.body.message) {
-      res.status(400).json({
-        errors: [
-          {
-            id: Date.now(),
-            status: '400',
-            title: 'Bad request',
-            detail: 'required fields must not be empty',
-          },
-        ],
-      });
-      return;
-    }
+  if (!req.body.name || !req.body.email || !req.body.message) {
+    res.status(400).json({
+      errors: [
+        {
+          id: Date.now(),
+          status: '400',
+          title: 'Bad request',
+          detail: 'required fields must not be empty',
+        },
+      ],
+    });
+    return;
+  }
 
-    const html = `Name: ${req.body.name}<br>Email: ${req.body.email}<br>${req.body.message}`;
-    const clearHtml = sanitizeHTML(html);
+  const html = `Name: ${req.body.name}<br>Email: ${req.body.email}<br>${req.body.message}`;
+  const clearHtml = sanitizeHTML(html);
 
-    const info = await transporter.sendMail({
+  transporter.sendMail(
+    {
       from: process.env.SOURCE,
       to: process.env.USERNAME,
       subject: 'Contact form message',
       text: clearHtml,
       html: clearHtml,
-    });
+    },
+    (err, info) => {
+      if (err) {
+        return res.status(500).json({
+          errors: [
+            {
+              id: Date.now(),
+              status: '500',
+              title: 'Internal server error',
+              detail: 'mailer could not send the message',
+            },
+          ],
+        });
+      }
 
-    res.status(200).json({
-      meta: {
-        data: `Message sent: ${info.messageId}`,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      errors: [
-        {
-          id: Date.now(),
-          status: '500',
-          title: 'Internal server error',
+      res.status(200).json({
+        meta: {
+          data: `Message sent: ${info.messageId}`,
         },
-      ],
-    });
-  }
+      });
+    }
+  );
 };
